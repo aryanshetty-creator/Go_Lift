ecc7c730565b91d0e352761c7b7aca2ad3728003
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-07 20:09:52 +0530

    Initial commit: Add Vue.js fitness tracker

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go Lift 💪
+
+A simple Vue.js fitness tracking application.
+
+## Features
+- Track your workouts
+- Simple and intuitive interface
+- Built with Vue 3
+
+## Getting Started
+Simply open `index.html` in your browser!
---
+++ app.js
@@ -0,0 +1,38 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            appName: 'Go Lift 💪'
+        }
+    }
+}).component('workout-tracker', {
+    template: `
+        <div class="workout-container">
+            <h2>Track Your Workouts</h2>
+            <div class="workout-form">
+                <input v-model="newWorkout" placeholder="Enter exercise name" />
+                <button @click="addWorkout">Add Workout</button>
+            </div>
+            <ul class="workout-list">
+                <li v-for="(workout, index) in workouts" :key="index">
+                    {{ workout }}
+                </li>
+            </ul>
+        </div>
+    `,
+    data() {
+        return {
+            newWorkout: '',
+            workouts: []
+        }
+    },
+    methods: {
+        addWorkout() {
+            if (this.newWorkout.trim()) {
+                this.workouts.push(this.newWorkout);
+                this.newWorkout = '';
+            }
+        }
+    }
+}).mount('#app');
