8658436ea90c3bd06a024dc68df45ad868792683
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-08 14:54:20 +0530

    Create exercise card component

---
+++ components/ExerciseCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="exercise-card">
+    <div class="exercise-header">
+      <h4>{{ exercise.name }}</h4>
+      <span class="exercise-type">{{ exercise.type }}</span>
+    </div>
+    <div class="exercise-details">
+      <span>Sets: {{ exercise.sets }}</span>
+      <span>Reps: {{ exercise.reps }}</span>
+      <span>Weight: {{ exercise.weight }}kg</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExerciseCard',
+  props: {
+    exercise: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
