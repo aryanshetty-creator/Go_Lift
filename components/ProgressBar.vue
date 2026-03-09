60a233cb448d75c18edaae601bd8f4771e07e2a2
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-09 21:32:01 +0530

    Add progress bar component for weekly goals

---
+++ components/ProgressBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="progress-container">
+    <div class="progress-label">
+      <span>Weekly Progress</span>
+      <span>{{ completed }}/{{ total }}</span>
+    </div>
+    <div class="progress-bar">
+      <div class="progress-fill" :style="{ width: progressPercent + '%' }"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressBar',
+  props: {
+    completed: {
+      type: Number,
+      default: 0
+    },
+    total: {
+      type: Number,
+      default: 7
+    }
+  },
+  computed: {
+    progressPercent() {
+      return Math.min((this.completed / this.total) * 100, 100);
+    }
+  }
+}
+</script>
