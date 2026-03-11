8f3c6ea7438c74a11bafdda70bb83c866b71766d
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-11 22:47:11 +0530

    Create muscle group selector component

---
+++ components/MuscleGroupSelector.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="muscle-selector">
+    <h4>Target Muscle Groups</h4>
+    <div class="muscle-grid">
+      <button 
+        v-for="muscle in muscleGroups" 
+        :key="muscle"
+        :class="['muscle-btn', { active: isSelected(muscle) }]"
+        @click="toggleMuscle(muscle)"
+      >
+        {{ muscle }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MuscleGroupSelector',
+  data() {
+    return {
+      muscleGroups: ['Chest', 'Back', 'Shoulders', 'Arms', 'Legs', 'Core'],
+      selectedMuscles: []
+    }
+  },
+  methods: {
+    toggleMuscle(muscle) {
+      const index = this.selectedMuscles.indexOf(muscle);
+      if (index > -1) {
+        this.selectedMuscles.splice(index, 1);
+      } else {
+        this.selectedMuscles.push(muscle);
+      }
+      this.$emit('update', this.selectedMuscles);
+    },
+    isSelected(muscle) {
+      return this.selectedMuscles.includes(muscle);
+    }
+  }
+}
+</script>
