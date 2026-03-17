c17bb9d421a40902755d8d8c78fb727f593ce63b
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-18 00:04:20 +0530

    Add workout companion component

---
+++ components/WorkoutCompanion.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="workout-companion">
+    <h3>Workout Companion</h3>
+    <div class="companion-status">
+      <p>Current Exercise: {{ currentExercise }}</p>
+      <p>Set {{ currentSet }} of {{ totalSets }}</p>
+      <div class="timer">{{ timer }}s</div>
+    </div>
+    <div class="companion-controls">
+      <button @click="startSet">Start Set</button>
+      <button @click="completeSet">Complete Set</button>
+      <button @click="skipExercise">Skip</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkoutCompanion',
+  data() {
+    return {
+      currentExercise: 'Bench Press',
+      currentSet: 1,
+      totalSets: 4,
+      timer: 0
+    }
+  },
+  methods: {
+    startSet() {
+      console.log('Set started');
+    },
+    completeSet() {
+      this.currentSet++;
+    },
+    skipExercise() {
+      this.currentSet = 1;
+    }
+  }
+}
+</script>
