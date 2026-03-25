8fd9b826b57006fdf27303b8b70f4fc5e2b890f7
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-26 00:28:46 +0530

    Add workout feedback component

---
+++ components/WorkoutFeedback.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="workout-feedback">
+    <h3>How was your workout?</h3>
+    <div class="rating">
+      <button v-for="n in 5" :key="n" @click="rate(n)">{{ n }}⭐</button>
+    </div>
+    <p v-if="rating">You rated: {{ rating }}/5</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutFeedback',
+  data() { return { rating: 0 } },
+  methods: { rate(n) { this.rating = n; } }
+}
+</script>
