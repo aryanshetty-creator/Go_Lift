3bd13ffdeb04280634e2bb10e158e9e91d334cd8
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-26 00:28:36 +0530

    Add heart rate monitor component

---
+++ components/HeartRateMonitor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="heart-rate">
+    <h3>Heart Rate Monitor</h3>
+    <p>Current HR: {{ heartRate }} bpm</p>
+    <p>Zone: {{ zone }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HeartRateMonitor',
+  data() { return { heartRate: 75 } },
+  computed: {
+    zone() {
+      if (this.heartRate < 100) return 'Rest';
+      if (this.heartRate < 140) return 'Fat Burn';
+      return 'Cardio';
+    }
+  }
+}
+</script>
