459f42bf9b8aea7df8b99a0745c8b708dd0b076c
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-26 00:28:26 +0530

    Create step counter component

---
+++ components/StepCounter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="step-counter">
+    <h3>Daily Steps</h3>
+    <p>{{ steps }} / 10,000</p>
+    <button @click="steps += 100">Add 100 Steps</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'StepCounter',
+  data() { return { steps: 0 } }
+}
+</script>
