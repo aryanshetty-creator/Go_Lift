eb95070ce62d9b47f61b5bd5b65643d17acfbfa1
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-26 00:28:41 +0530

    Create progress photos component

---
+++ components/ProgressPhotos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="progress-photos">
+    <h3>Progress Photos</h3>
+    <input type="file" @change="uploadPhoto" />
+    <div class="photos">
+      <img v-for="(photo, i) in photos" :key="i" :src="photo" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProgressPhotos',
+  data() { return { photos: [] } },
+  methods: {
+    uploadPhoto(e) {
+      const file = e.target.files[0];
+      if (file) this.photos.push(URL.createObjectURL(file));
+    }
+  }
+}
+</script>
