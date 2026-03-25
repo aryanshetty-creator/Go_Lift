8af5693144b732166a95260c87dc9e335a828641
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-26 00:28:32 +0530

    Add workout notes component

---
+++ components/WorkoutNotes.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="workout-notes">
+    <h3>Workout Notes</h3>
+    <textarea v-model="notes" placeholder="Add notes..."></textarea>
+    <button @click="save">Save</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutNotes',
+  data() { return { notes: '' } },
+  methods: { save() { localStorage.setItem('notes', this.notes); } }
+}
+</script>
