e3704192a0ca8f3a2cb23e70b3643a51f7185ca8
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-26 00:28:22 +0530

    Create weekly goals component

---
+++ components/WeeklyGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="weekly-goals">
+    <h3>Weekly Goals</h3>
+    <input v-model="goal" placeholder="Enter goal" />
+    <button @click="addGoal">Add</button>
+    <ul>
+      <li v-for="(g, i) in goals" :key="i">{{ g }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WeeklyGoals',
+  data() { return { goal: '', goals: [] } },
+  methods: { addGoal() { this.goals.push(this.goal); this.goal = ''; } }
+}
+</script>
