c7995f41a58bb4b129bdda040fb67d3719ee159f
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-26 23:16:13 +0530

    Create workout reminders component

---
+++ components/WorkoutReminders.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="workout-reminders">
+    <h3>Workout Reminders</h3>
+    <div class="reminder-setup">
+      <input v-model="time" type="time" />
+      <button @click="setReminder">Set Reminder</button>
+    </div>
+    <p v-if="reminderSet">Reminder set for {{ time }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutReminders',
+  data() {
+    return { time: '09:00', reminderSet: false }
+  },
+  methods: {
+    setReminder() {
+      this.reminderSet = true;
+    }
+  }
+}
+</script>
