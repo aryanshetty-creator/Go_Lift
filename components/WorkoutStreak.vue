48c0eed41ef2f467a4fd12ec17a76c74e3844990
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-26 23:15:55 +0530

    Create workout streak display component

---
+++ components/WorkoutStreak.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="workout-streak">
+    <h3>Current Streak</h3>
+    <div class="streak-display">
+      <span class="streak-number">{{ streak }}</span>
+      <span class="streak-label">days</span>
+    </div>
+    <p>Keep it up! 🔥</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutStreak',
+  data() {
+    return { streak: 7 }
+  }
+}
+</script>
