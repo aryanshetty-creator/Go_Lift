ee2644e462faa8bfdb6049366c2bb66c76d2e7f7
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-27 23:24:12 +0530

    Create muscle map visualization component

---
+++ components/MuscleMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="muscle-map">
+    <h3>Muscle Map</h3>
+    <div class="body-diagram">
+      <div v-for="muscle in muscles" :key="muscle.name" 
+           :class="['muscle', { worked: muscle.worked }]">
+        {{ muscle.name }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MuscleMap',
+  data() {
+    return {
+      muscles: [
+        { name: 'Chest', worked: true },
+        { name: 'Back', worked: false },
+        { name: 'Legs', worked: true }
+      ]
+    }
+  }
+}
+</script>
