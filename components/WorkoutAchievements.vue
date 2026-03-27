8d9b1c97c9959f67243fc0196e561a39bd6293ec
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-27 23:24:30 +0530

    Create workout achievements component

---
+++ components/WorkoutAchievements.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="achievements">
+    <h3>Achievements</h3>
+    <div class="achievement-grid">
+      <div v-for="achievement in achievements" :key="achievement.id"
+           :class="['achievement', { unlocked: achievement.unlocked }]">
+        <span class="icon">{{ achievement.icon }}</span>
+        <p>{{ achievement.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutAchievements',
+  data() {
+    return {
+      achievements: [
+        { id: 1, name: 'First Workout', icon: '🎯', unlocked: true },
+        { id: 2, name: '7 Day Streak', icon: '🔥', unlocked: false },
+        { id: 3, name: '100 Workouts', icon: '💯', unlocked: false }
+      ]
+    }
+  }
+}
+</script>
