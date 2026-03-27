dd9752c5485659f15ea403a93a8bb490f57873b9
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-27 23:24:18 +0530

    Create quick workout guide component

---
+++ components/QuickWorkout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="quick-workout">
+    <h3>Quick Workout</h3>
+    <p>{{ currentExercise }}</p>
+    <button @click="nextExercise">Next Exercise</button>
+    <p>{{ exerciseIndex + 1 }} / {{ exercises.length }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'QuickWorkout',
+  data() {
+    return {
+      exerciseIndex: 0,
+      exercises: ['Push-ups', 'Squats', 'Plank', 'Lunges']
+    }
+  },
+  computed: {
+    currentExercise() {
+      return this.exercises[this.exerciseIndex];
+    }
+  },
+  methods: {
+    nextExercise() {
+      this.exerciseIndex = (this.exerciseIndex + 1) % this.exercises.length;
+    }
+  }
+}
+</script>
