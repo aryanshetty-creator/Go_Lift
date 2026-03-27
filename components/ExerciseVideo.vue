648fac964af6eb783d9f7901861f6d6a651fea66
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-27 23:51:13 +0530

    Create exercise video tutorial component

---
+++ components/ExerciseVideo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="exercise-video">
+    <h3>Exercise Videos</h3>
+    <select v-model="selectedExercise">
+      <option v-for="ex in exercises" :key="ex" :value="ex">{{ ex }}</option>
+    </select>
+    <div class="video-placeholder">
+      <p>Video tutorial for {{ selectedExercise }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ExerciseVideo',
+  data() {
+    return {
+      selectedExercise: 'Squat',
+      exercises: ['Squat', 'Bench Press', 'Deadlift', 'Pull-up']
+    }
+  }
+}
+</script>
