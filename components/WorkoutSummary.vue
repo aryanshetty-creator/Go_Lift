9ebb1f7a6bd929e98ffb1b2bbec4167618b9ff03
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-27 23:24:06 +0530

    Create workout summary component

---
+++ components/WorkoutSummary.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="workout-summary">
+    <h3>Today's Summary</h3>
+    <div class="summary-stats">
+      <p>Exercises: {{ exerciseCount }}</p>
+      <p>Total Sets: {{ totalSets }}</p>
+      <p>Duration: {{ duration }} min</p>
+      <p>Calories: {{ calories }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutSummary',
+  data() {
+    return { exerciseCount: 5, totalSets: 20, duration: 60, calories: 400 }
+  }
+}
+</script>
