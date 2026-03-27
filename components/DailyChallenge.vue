5b57fa1cf07107b9ae3c59e2d749f9a34dbde00f
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-27 23:24:36 +0530

    Create daily challenge component

---
+++ components/DailyChallenge.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="daily-challenge">
+    <h3>Daily Challenge</h3>
+    <div class="challenge">
+      <h4>{{ challenge.name }}</h4>
+      <p>{{ challenge.description }}</p>
+      <button @click="complete">Complete Challenge</button>
+    </div>
+    <p v-if="completed">✅ Challenge completed!</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DailyChallenge',
+  data() {
+    return {
+      completed: false,
+      challenge: {
+        name: '100 Push-ups',
+        description: 'Complete 100 push-ups today'
+      }
+    }
+  },
+  methods: {
+    complete() {
+      this.completed = true;
+    }
+  }
+}
+</script>
