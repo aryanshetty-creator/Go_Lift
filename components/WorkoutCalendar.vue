9345f69c22a96cf54f792e9094cac4b808386492
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-27 23:50:59 +0530

    Create workout calendar component

---
+++ components/WorkoutCalendar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="workout-calendar">
+    <h3>Workout Calendar</h3>
+    <div class="calendar">
+      <div v-for="day in days" :key="day" 
+           :class="['day', { workout: day.hasWorkout }]">
+        {{ day.date }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutCalendar',
+  data() {
+    return {
+      days: Array.from({length: 30}, (_, i) => ({
+        date: i + 1,
+        hasWorkout: Math.random() > 0.5
+      }))
+    }
+  }
+}
+</script>
