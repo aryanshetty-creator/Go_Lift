d2ecd4d0f92ca4f138517dab60cca0a5f4797075
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-29 01:40:46 +0530

    Create body metrics display component

---
+++ components/BodyMetrics.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="body-metrics">
+    <h3>Body Metrics</h3>
+    <div class="metrics">
+      <p>BMI: {{ bmi }}</p>
+      <p>Body Fat: {{ bodyFat }}%</p>
+      <p>Lean Mass: {{ leanMass }}kg</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'BodyMetrics',
+  data() {
+    return { bmi: 22.5, bodyFat: 15, leanMass: 65 }
+  }
+}
+</script>
