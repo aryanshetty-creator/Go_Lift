85613ca16a9f1d96f4d5faa21d97db189745cbde
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-29 01:40:59 +0530

    Create workout tips component

---
+++ components/WorkoutTips.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="workout-tips">
+    <h3>Workout Tips</h3>
+    <div class="tip">
+      <p>{{ currentTip }}</p>
+      <button @click="nextTip">Next Tip</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutTips',
+  data() {
+    return {
+      tipIndex: 0,
+      tips: [
+        'Stay hydrated during workouts',
+        'Warm up before exercising',
+        'Focus on form over weight'
+      ]
+    }
+  },
+  computed: {
+    currentTip() {
+      return this.tips[this.tipIndex];
+    }
+  },
+  methods: {
+    nextTip() {
+      this.tipIndex = (this.tipIndex + 1) % this.tips.length;
+    }
+  }
+}
+</script>
