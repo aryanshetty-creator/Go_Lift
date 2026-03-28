db55a31b9bef0a935749b563662f7dff76a82a35
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-29 01:40:54 +0530

    Create fitness goals component

---
+++ components/FitnessGoals.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="fitness-goals">
+    <h3>Fitness Goals</h3>
+    <input v-model="newGoal" placeholder="Enter goal" />
+    <button @click="addGoal">Add Goal</button>
+    <ul>
+      <li v-for="goal in goals" :key="goal">{{ goal }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FitnessGoals',
+  data() {
+    return { newGoal: '', goals: [] }
+  },
+  methods: {
+    addGoal() {
+      if (this.newGoal) {
+        this.goals.push(this.newGoal);
+        this.newGoal = '';
+      }
+    }
+  }
+}
+</script>
