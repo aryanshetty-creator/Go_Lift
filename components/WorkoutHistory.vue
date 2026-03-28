9c835b46d7e50c672c231cf13bcb281e1213503c
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-29 01:40:50 +0530

    Create workout history component

--- components/WorkoutHistory.vue
+++ components/WorkoutHistory.vue
@@ -1,34 +1,19 @@
 <template>
-  <div class="history-container">
+  <div class="workout-history">
     <h3>Workout History</h3>
-    <div v-if="history.length === 0" class="empty-state">
-      <p>No workout history yet. Start tracking!</p>
+    <div v-for="workout in history" :key="workout.id">
+      <p>{{ workout.date }}: {{ workout.type }}</p>
     </div>
-    <ul v-else class="history-list">
-      <li v-for="(entry, index) in history" :key="index" class="history-item">
-        <div class="history-date">{{ formatDate(entry.date) }}</div>
-        <div class="history-workout">{{ entry.workout }}</div>
-        <div class="history-duration">{{ entry.duration }} min</div>
-      </li>
-    </ul>
   </div>
 </template>
-
 <script>
 export default {
   name: 'WorkoutHistory',
-  props: {
-    history: {
-      type: Array,
-      default: () => []
-    }
-  },
-  methods: {
-    formatDate(date) {
-      return new Date(date).toLocaleDateString('en-US', { 
-        month: 'short', 
-        day: 'numeric' 
-      });
+  data() {
+    return {
+      history: [
+        { id: 1, date: '2024-01-15', type: 'Upper Body' }
+      ]
     }
   }
 }
