d3045bdd4e2ccb5c23ab741e379e6909af19e07c
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-29 01:40:38 +0530

    Create exercise alternatives component

---
+++ components/ExerciseAlternatives.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exercise-alternatives">
+    <h3>Exercise Alternatives</h3>
+    <input v-model="exercise" placeholder="Enter exercise" />
+    <div v-if="alternatives">
+      <p v-for="alt in alternatives" :key="alt">{{ alt }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ExerciseAlternatives',
+  data() {
+    return { exercise: '', alternatives: [] }
+  }
+}
+</script>
