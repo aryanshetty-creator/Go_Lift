a3a1bc930418fee38a678b2d5278978561077b04
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-29 01:40:34 +0530

    Create workout badges component

---
+++ components/WorkoutBadges.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="workout-badges">
+    <h3>Your Badges</h3>
+    <div class="badges">
+      <div v-for="badge in badges" :key="badge.id" class="badge">
+        {{ badge.icon }} {{ badge.name }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutBadges',
+  data() {
+    return {
+      badges: [
+        { id: 1, icon: '🏆', name: 'Champion' },
+        { id: 2, icon: '🔥', name: 'On Fire' }
+      ]
+    }
+  }
+}
+</script>
