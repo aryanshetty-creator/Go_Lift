be8849d48409ebd038ff5f6764f7d0dee590b023
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-29 01:40:43 +0530

    Create workout recommendations component

---
+++ components/WorkoutRecommendations.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="workout-recommendations">
+    <h3>Recommended Workouts</h3>
+    <div v-for="workout in recommendations" :key="workout.id">
+      <h4>{{ workout.name }}</h4>
+      <p>{{ workout.description }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutRecommendations',
+  data() {
+    return {
+      recommendations: [
+        { id: 1, name: 'Upper Body', description: 'Focus on chest and back' }
+      ]
+    }
+  }
+}
+</script>
