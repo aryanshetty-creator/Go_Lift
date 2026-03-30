ebc9f15d671c1dea914a0a1b5ad2b7d94bab0d5b
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-30 20:01:05 +0530

    Create workout duration tracker

---
+++ components/WorkoutDuration.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="workout-duration">
+    <h3>Workout Duration</h3>
+    <p>{{ hours }}h {{ minutes }}m</p>
+    <button @click="start">Start</button>
+    <button @click="stop">Stop</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutDuration',
+  data() {
+    return { hours: 0, minutes: 0 }
+  },
+  methods: {
+    start() {
+      console.log('Started');
+    },
+    stop() {
+      console.log('Stopped');
+    }
+  }
+}
+</script>
