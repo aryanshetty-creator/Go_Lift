6b491e2185149aff310dad3e89c28a291ef99dc8
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-30 20:00:40 +0530

    Create workout challenge tracker

---
+++ components/WorkoutChallenge.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="workout-challenge">
+    <h3>Weekly Challenge</h3>
+    <p>{{ challenge.name }}</p>
+    <p>Progress: {{ progress }}%</p>
+    <button @click="updateProgress">Update</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutChallenge',
+  data() {
+    return {
+      challenge: { name: '100 Push-ups Challenge' },
+      progress: 0
+    }
+  },
+  methods: {
+    updateProgress() {
+      this.progress = Math.min(100, this.progress + 10);
+    }
+  }
+}
+</script>
