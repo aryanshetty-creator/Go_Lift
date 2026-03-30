3acf943d50ee4548048b5b3062cb343fe7c68bd1
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-30 20:00:58 +0530

    Create workout equipment list component

---
+++ components/WorkoutEquipment.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="workout-equipment">
+    <h3>Equipment Needed</h3>
+    <ul>
+      <li v-for="item in equipment" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutEquipment',
+  data() {
+    return {
+      equipment: ['Dumbbells', 'Bench', 'Resistance Bands']
+    }
+  }
+}
+</script>
