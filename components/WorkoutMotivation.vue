244971debb952cc4d36ac137d3d016872ab4e46f
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-30 20:00:49 +0530

    Create workout motivation component

---
+++ components/WorkoutMotivation.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="workout-motivation">
+    <h3>Stay Motivated!</h3>
+    <p class="quote">"{{ quote }}"</p>
+    <button @click="newQuote">New Quote</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutMotivation',
+  data() {
+    return {
+      quoteIndex: 0,
+      quotes: [
+        'No pain, no gain',
+        'Train insane or remain the same',
+        'Your only limit is you'
+      ]
+    }
+  },
+  computed: {
+    quote() {
+      return this.quotes[this.quoteIndex];
+    }
+  },
+  methods: {
+    newQuote() {
+      this.quoteIndex = (this.quoteIndex + 1) % this.quotes.length;
+    }
+  }
+}
+</script>
