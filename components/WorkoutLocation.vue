31fd912a97dbd52551179a8956a03bba5c10cdb0
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-30 20:01:01 +0530

    Create workout location selector

---
+++ components/WorkoutLocation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="workout-location">
+    <h3>Workout Location</h3>
+    <select v-model="location">
+      <option value="gym">Gym</option>
+      <option value="home">Home</option>
+      <option value="outdoor">Outdoor</option>
+    </select>
+    <p>Selected: {{ location }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutLocation',
+  data() {
+    return { location: 'gym' }
+  }
+}
+</script>
