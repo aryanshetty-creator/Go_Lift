bc598f0a8d0af0400f6935e5956a90993b70a26a
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-30 20:01:10 +0530

    Create workout intensity slider

---
+++ components/WorkoutIntensity.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="workout-intensity">
+    <h3>Workout Intensity</h3>
+    <input type="range" v-model="intensity" min="0" max="100" />
+    <p>{{ intensity }}%</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutIntensity',
+  data() {
+    return { intensity: 50 }
+  }
+}
+</script>
