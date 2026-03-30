59dc78a29272770c86ad1b9ec73cfae65ce3266e
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-30 20:00:53 +0530

    Create exercise guide component

---
+++ components/ExerciseGuide.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="exercise-guide">
+    <h3>Exercise Guide</h3>
+    <select v-model="selected">
+      <option v-for="ex in exercises" :key="ex.id" :value="ex">
+        {{ ex.name }}
+      </option>
+    </select>
+    <div v-if="selected">
+      <p>{{ selected.instructions }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ExerciseGuide',
+  data() {
+    return {
+      selected: null,
+      exercises: [
+        { id: 1, name: 'Squat', instructions: 'Stand with feet shoulder-width apart...' }
+      ]
+    }
+  }
+}
+</script>
