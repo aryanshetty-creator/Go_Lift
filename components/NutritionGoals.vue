b430ba13ca12f27beb8b66c0a8e5ca93bedd0cb2
aryanshetty-creator <aryanshetty546@gmail.com>
2026-03-30 20:00:44 +0530

    Create nutrition goals tracker

---
+++ components/NutritionGoals.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nutrition-goals">
+    <h3>Nutrition Goals</h3>
+    <div class="goals">
+      <p>Calories: {{ calories }}/{{ calorieGoal }}</p>
+      <p>Protein: {{ protein }}g/{{ proteinGoal }}g</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NutritionGoals',
+  data() {
+    return {
+      calories: 1500,
+      calorieGoal: 2000,
+      protein: 80,
+      proteinGoal: 150
+    }
+  }
+}
+</script>
